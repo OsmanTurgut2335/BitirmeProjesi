bcb019d4edfd3488f41d6350b3490f76f8704c66
Osman <oeturgut3542@hotmail.com>
2024-09-11 02:17:41 +0300

    initial commit

---
+++ app/src/main/java/com/osman/bitirmeprojesi/MainScreen.kt
@@ -0,0 +1,39 @@
+package com.osman.bitirmeprojesi
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+
+            )
+        },
+
+    ){paddingValues ->
+
+
+    }
+}
